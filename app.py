ceefa62a91947d4450537c24d2db7f48d9706767
RamsesHrndz <al25220410@utcj.edu.mx>
2026-06-25 21:11:23 -0600

    Railway port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from calculator import sumar, restar, multiplicar, dividir
 
@@ -27,4 +28,5 @@ def index():
     return render_template("index.html", resultado=resultado, error=error)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
